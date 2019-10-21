b7d2bcddc0378aa9cad48dba120f05a4613afaf3
JustWh0 <47599029+Nex123@users.noreply.github.com>
2019-10-21 20:06:52 +0200

    Update crawler.py
    Found a way to get the csrf token

--- crawler.py
+++ crawler.py
@@ -1,31 +1,33 @@
 import requests
+import json
+import re
+from bs4 import BeautifulSoup
 
 class Crawler:
 	def __init__(self, username, password):
 		self.username = username
 		self.password = password
-		self.userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"
 		self.baseUrl = "https://www.instagram.com"
-		self.loginUrl = self.baseUrl + "/accounts/login"       
-
-	def login(self):
-		session = requests.Session()
-		csrf = session.get(self.loginUrl)
-		loginCsrfToken = str(csrf.cookies)
-		requestHeaders = {
-							"origin":self.baseUrl,
-							"accept-encoding": "gzip, deflate, br",
-							"accept-language": "en-US,en;q=0.9",
-							"user-agent": self.userAgent,
-							"x-requested-with": "XMLHttpRequest",
-							"x-csrftoken": loginCsrfToken,
-							"x-instagram-ajax": "de81cb3fd9c4-hot",
-							"content-type": "application/x-www-form-urlencoded",
-							"accept": "*/*",
-							"referer": "https://www.instagram.com/accounts/login"
+		self.loginUrl = self.baseUrl + "/accounts/login"
+		self.userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0"
+		self.loginData = {
+						"username": self.username,
+						"password": self.password
 		}
-		login = session.post(self.loginUrl, data={"username": self.username, "password":self.password}, headers=requestHeaders)
-		if 'authenticated": true,' in str(login.text):
-c = Crawler("pythontest53", "testtest") 
-c.login()
-#i don't know what is wrong but the csrftoken doesn't seem to get fetched
+		self.csrfToken = None
+
+	def getCsrfToken(self):
+		with requests.Session() as s:
+			s.headers = {"user-agent": self.userAgent}
+			s.headers.update({"Referer": self.baseUrl})
+
+			get = s.get(self.baseUrl)
+
+			soup = BeautifulSoup(get.content, "html.parser")
+			body = soup.find("body")
+
+			js = body.find("script", text=re.compile("window._sharedData"))
+			js = js.get_text().replace("window._sharedData = ", "")[:-1]
+			jsonData = json.loads(js)
+			self.csrfToken = jsonData["config"]["csrf_token"]
+			return self.csrfToken
